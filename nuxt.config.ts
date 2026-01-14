83cce404f6c30e65234d004944d5460e845d0906
imprvhub <ivanflowtramites@gmail.com>
2026-01-14 00:36:50 -0300

    perf: drop console and debugger statements in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,6 +76,9 @@ export default defineNuxtConfig({
     define: {
       'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
       'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY)
+    },
+    esbuild: {
+      drop: ['console', 'debugger'],
     }
   }
 })
