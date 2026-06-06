32eb98c6a5b1ed3a91399d3b135730ce0ce2654f
imprvhub <ivanflowtramites@gmail.com>
2026-06-06 17:30:40 -0300

    fix(ui): bust noir modal logo cache via v2 filename
    Rename /ui/noir-selection-500x500.svg → noir-selection-500x500-v2.svg
    y update NoirModal.vue. Mismo patrón que el .webp del badge en Hero.

--- components/NoirModal.vue
+++ components/NoirModal.vue
@@ -11,7 +11,7 @@
 
       <div class="noir-modal-body">
         <div class="noir-modal-header">
-          <img src="/ui/noir-selection-500x500.svg" alt="N.O.I.R" class="noir-modal-logo" />
+          <img src="/ui/noir-selection-500x500-v2.svg" alt="N.O.I.R" class="noir-modal-logo" />
           <p class="noir-modal-subtitle">Nothing Out Is Ready</p>
         </div>
 
